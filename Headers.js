9c85a5c3ef507c0edab6269bc16158ebfa4252ac
Evelyn Koo <82856360+randomrevised@users.noreply.github.com>
2021-05-26 16:59:06 -0700

    edited headers

--- Headers.js
+++ Headers.js
@@ -1,13 +1,13 @@
 var nav = `<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 <div class="container">
-  <a class="navbar-brand" href="Index.html">Competitor.</a>
+  <a class="navbar-brand" href="index.html">Competitor.</a>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarText">
     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
       <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="Index.html">Home</a>
+        <a class="nav-link active" aria-current="page" href="index.html">Home</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
