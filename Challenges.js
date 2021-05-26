4173a27695cbfd7b5385b6977b886ae193793941
Evelyn Koo <82856360+randomrevised@users.noreply.github.com>
2021-05-26 16:20:07 -0700

    added inital stage of form

--- Challenges.js
+++ Challenges.js
@@ -1,26 +1,5 @@
 // This will be so messy :X
 
-
-// Challenges
-challenge1 = "This is a test challenge."
-challenge2 = 
-challenge3 = 
-challenge4 = 
-challenge5 = 
-challenge6 = 
-challenge7 = 
-challenge8 = 
-challenge9 = 
-challenge10 = 
-challenge11 = 
-challenge12 = 
-
-function challenges(element,chall="Coming Soon"){
-    document,getElementById(element).innerHTML = chall
-}
-
-
-
 // Prizes
 prizetypes = ['crystals <img src="https://i.imgur.com/ckBFg6Z.png">','Containers <img src="https://i.imgur.com/ChiyPgD.png">','Gold Boxes <img src="https://i.imgur.com/X2cDeUb.png?1">','Tankoins <img src="https://i.imgur.com/724Ay0Y.png">','Days of Premium  <img src="https://i.imgur.com/ScwXWQl.png">']
 
@@ -47,12 +26,17 @@ prizes("prize10",5,prizetypes[3])
 prizes("prize11",5,prizetypes[1])
 prizes("prize12",5,prizetypes[2]) // Challenge 12
 
+
 // Organizer Tab
 organizers = ['Dynasty','Person_Random','Venerable']
 
 function organizer(element,name='Dynasty'){ // Sets default value to Dynasty
     document.getElementById(element).innerHTML=name
 }
+
+organizer1 = "kaisdf"
+
+organizer("organizer1",organizer1)
 
 // Organizers
 
@@ -87,3 +71,30 @@ statusbar("stat10",statusmenu[1])
 statusbar("stat11",statusmenu[2])
 statusbar("stat12",statusmenu[2]) // Challenge 12
 
+// Challenges Tab - Controls both Form and Table :relieved:
+function setchall(element,name='Coming Soon - To be determined'){ // Sets default value to TBD
+    document.getElementById(element).innerHTML=name
+}
+
+// Set the Challenges - MAKE SURE TO ENCLOSE THEM IN QUOTES
+var challenge1 = "This is a test challenge."
+var challenge2 = "Test"
+var challenge3 = "Test"
+var challenge4 = "Test"
+var challenge5 = "Test"
+var challenge6 = "Test"
+var challenge7 = "Test"
+var challenge8 = "Test"
+var challenge9 = "Test"
+var challenge10 = "Test"
+var challenge11 = "Test"
+var challenge12 = "Test"
+
+challengeArray = [challenge1,challenge2,challenge3,challenge4,challenge5,challenge6,challenge7,challenge8,challenge9,challenge10,challenge11,challenge12]
+
+// Loop that sets the challenges in the form and table
+i=0
+while (i<12){
+    setchall("obj"+(i+1),challengeArray[i]);
+    i++;
+}
--- Headers.js
+++ Headers.js
@@ -15,7 +15,7 @@ var nav = `<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         </a>
         <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
           <li><a class="dropdown-item" href="Tables.html">Current Challenges</a></li>
-          <li><a class="dropdown-item" href="#">Form</a></li>
+          <li><a class="dropdown-item" href="Forms.html">Form</a></li>
         </ul>
       </li>
       <li class="nav-item">
@@ -32,7 +32,6 @@ var nav = `<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
 styles = `<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-<link rel = "stylesheet" type = "text/css" href = "tables.css" />
 <link rel = "stylesheet" type = "text/css" href = "Zero.css"/>`
 
 title_img = '<img class="border" style="width: 100%" src="https://i.imgur.com/62lbngm.jpg">'
