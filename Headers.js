138be1aa665b23e43e35bf2220bcde6685940c99
Evelyn Koo <82856360+randomrevised@users.noreply.github.com>
2021-05-26 16:54:18 -0700

    added site icon

--- Headers.js
+++ Headers.js
@@ -30,7 +30,8 @@ var nav = `<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 </nav>
 `
 
-styles = `<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+styles = `<link rel="icon" href="https://tankionline.com/en/wp-content/uploads/2019/08/wf.png">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 <link rel = "stylesheet" type = "text/css" href = "Zero.css"/>`
 
